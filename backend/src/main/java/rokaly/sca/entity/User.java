922021e599d79f790c0b3829cbbb349663d9b351
Vitor Mateus <vmateusaguiar@gmail.com>
2026-01-16 22:45:36 -0300

    feat: create users/products/positions tables

--- backend/src/main/java/rokaly/sca/entity/User.java
+++ backend/src/main/java/rokaly/sca/entity/User.java
@@ -1,15 +1,26 @@
 package rokaly.sca.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import rokaly.sca.utils.enums.Role;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(length = 25, nullable = false)
     private Role role;
 }
