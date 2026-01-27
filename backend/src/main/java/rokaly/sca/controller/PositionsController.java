e9530234ebe7b04ec88efd5ecff8d7fc50095bc2
Vitor Mateus <vmateusaguiar@gmail.com>
2026-01-27 10:25:14 -0300

    feat: get all positions

--- backend/src/main/java/rokaly/sca/controller/PositionsController.java
+++ backend/src/main/java/rokaly/sca/controller/PositionsController.java
@@ -2,11 +2,11 @@ package rokaly.sca.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import rokaly.sca.dto.PositionsRequest;
 import rokaly.sca.dto.PositionsResponse;
@@ -26,5 +26,10 @@ public class PositionsController {
     @Transactional
     public ResponseEntity<PositionsResponse> createPosition(@RequestBody @Valid PositionsRequest data, UriComponentsBuilder uriBuilder) {
         return positionsService.createPositionService(data, uriBuilder);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PositionsResponse>> getAll(@PageableDefault(size = 10, page = 0, sort = {"id"}) Pageable pagination) {
+        return positionsService.getAllService(pagination);
     }
 }
--- backend/src/main/java/rokaly/sca/service/PositionsService.java
+++ backend/src/main/java/rokaly/sca/service/PositionsService.java
@@ -1,11 +1,12 @@
 package rokaly.sca.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 import rokaly.sca.dto.PositionsRequest;
 import rokaly.sca.dto.PositionsResponse;
-import rokaly.sca.dto.ProductResponse;
 import rokaly.sca.entity.Positions;
 import rokaly.sca.repository.PositionRepository;
 
@@ -24,5 +25,10 @@ public class PositionsService {
         var uri = uriBuilder.path("/positions/{id}").buildAndExpand(positions.getId()).toUri();
         PositionsResponse dto = new PositionsResponse(positions.getId(), data.code(), positions.getStatus().getStatusPT());
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    public ResponseEntity<Page<PositionsResponse>> getAllService(Pageable pagination) {
+        Page<PositionsResponse> page = repository.findAll(pagination).map(p -> new PositionsResponse(p.getId(), p.getCode(), p.getStatus().getStatusPT()));
+        return ResponseEntity.ok(page);
     }
 }
